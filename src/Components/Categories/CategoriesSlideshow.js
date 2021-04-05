81cffd9c2e8488b99d0a78433794714603610522
harshika6 <harshikasmishra@gmail.com>
2021-04-05 17:10:02 +0530

    categories carousel added

--- src/Components/Categories/CategoriesSlideshow.js
+++ src/Components/Categories/CategoriesSlideshow.js
@@ -1,38 +1,56 @@
 import React, {useState} from 'react';
 import ImageGallery from 'react-image-gallery';
+import 'tachyons';
 import "react-image-gallery/styles/scss/image-gallery.scss";
 import "react-image-gallery/styles/css/image-gallery.css";
 
-const images = [
-  {
-    original: 'https://picsum.photos/id/1018/1000/400/',
-    thumbnail: 'https://picsum.photos/id/1018/1000/1000/',
-		description:'KITCHEN',
-		thumbnailLabel:'KITCHEN',
-		link:"https://www.npmjs.com/package/react-image-gallery"
-  },
-  {
-    original: 'https://picsum.photos/id/1015/1000/400/',
-    thumbnail: 'https://picsum.photos/id/1015/1000/1000/',
-		description:'BEDROOM',
-		thumbnailLabel:'BEDROOM',
-		link:"https://www.npmjs.com/package/react-image-gallery"
 
-  },
-  {
-    original: 'https://picsum.photos/id/1019/1000/400/',
-    thumbnail: 'https://picsum.photos/id/1019/1000/1000/',
-		description:'OFFICE',
-		thumbnailLabel:'OFFICE',
-		link:"https://www.npmjs.com/package/react-image-gallery"
+class CategoriesSlideshow extends React.Component {
 
-  },
-	{
+  _handleImageError(event) {
+    if (this.props.defaultImage &&
+        event.target.src.indexOf(this.props.defaultImage) === -1) {
+      event.target.src = this.props.defaultImage;
+    }
+  }
+
+  _myImageGalleryRenderer(item) {
+    const onImageError = this._handleImageError
+
+    return (
+      <div className='image-gallery-image flex'>
+
+        <a href={item.link} style={{height:'80%',width:'50%'}} >
+           <img
+            src={item.original}
+            alt={item.originalAlt}
+            srcSet={item.srcSet}
+            sizes={item.sizes}
+            onLoad={this.props.onImageLoad}
+            onError={onImageError.bind(this)}/>
+        </a>
+        <p className="center">{item.des}</p>
+      </div>
+    )
+  }
+
+  render() {
+    const items = [
+     {
+        original: "https://picsum.photos/id/1001/1000/400/",
+        thumbnail: 'https://picsum.photos/id/1001/1000/1000/',
+        description: 'KITCHEN',
+        thumbnailLabel:'KITCHEN',
+        link: 'http://example.com',
+        des:'We design kitchens'
+      },
+      {
     original: 'https://picsum.photos/id/1012/1000/400/',
     thumbnail: 'https://picsum.photos/id/1012/1000/1000/',
 		description:'HOTEL',
 		thumbnailLabel:'HOTEL',
-		link:"https://www.npmjs.com/package/react-image-gallery"
+		link:"https://www.npmjs.com/package/react-image-gallery",
+    des:'We design hotels'
 
   },
 	{
@@ -40,14 +58,19 @@ const images = [
     thumbnail: 'https://picsum.photos/id/1001/1000/1000/',
 		description:'BEDROOM',
 		thumbnailLabel:'BEDROOM',
-		link:"https://www.npmjs.com/package/react-image-gallery"
+		link:"https://www.npmjs.com/package/react-image-gallery",
+    des:'We design bedrooms'
 
   },
-];
-class CategoriesSlideshow extends React.Component {
-  render() {
-    return <ImageGallery items={images} lazyLoad={true} showNav={false}
-		showFullscreenButton={false} showPlayButton={false} onClick={()=> window.open(`${images[0].link}`, "_parent")} />;
+    ]
+    return(
+      <div>
+      <h1>Categories</h1>
+      <ImageGallery items={items} azyLoad={true} showNav={false}
+		showFullscreenButton={false} showPlayButton={false} renderItem={this._myImageGalleryRenderer.bind(this)}/>
+    </div>
+  );
   }
+
 }
 export default CategoriesSlideshow;
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ImageSlider from '../../Components/ImageSlider/ImageSlider.js';
+import CategoriesSlideshow from '../../Components/Categories/CategoriesSlideshow.js';
 import './HomePage.scss';
 
 const HomePage = () => {
 	return (
 		<div className="home-container">
 			<ImageSlider />
+			<CategoriesSlideshow/>
 		</div>
 	)
 }
