56bef758d89a3bda620a9b8ec6e3108f2736af79
tejasjokin <61027009+tejasjokin@users.noreply.github.com>
2021-04-07 16:09:44 +0530

    Adding footer component

---
+++ src/Components/Footer/ContactUs/ContactUs.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import FormInput from '../../FormInput/FormInput.js';
+import './ContactUs.scss';
+
+
+const initialState = {
+	name: '',
+	email: '',
+	message: ''
+}
+
+class ContactUs extends React.PureComponent {
+	constructor(){
+		super();
+		this.state = initialState;
+	}
+
+	handleSubmit = (event) =>
+	{
+		event.prefentDefault();
+		console.log(this.state);
+
+		this.setState({name: '', email: '', message: ''});
+	}
+
+	handleChange = (event) => {
+		const {value, name} = event.target;
+		this.this.setState({[name]: value});
+	}
+
+	render() {
+		return (
+			<div className="contact-container">
+				<p className="title">Contact Us</p>
+				<form onSubmit={this.handleSubmit}>
+					<FormInput 
+					label="Name"
+					name="name" 
+					type="text" 
+					value={this.state.name} 
+					handleChange={this.handleChange}
+					required
+					/>
+					<FormInput 
+					label="Email"
+					name="email" 
+					type="email" 
+					value={this.state.email} 
+					handleChange={this.handleChange}
+					required
+					/>
+					<FormInput 
+					label="Message"
+					name="message" 
+					type="text" 
+					value={this.state.message} 
+					handleChange={this.handleChange}
+					required
+					/>
+				</form>
+			</div>
+		)
+	}
+}
+
+export default ContactUs;
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Connect from './Connect/Connect.js';
+import ContactUs from './ContactUs/ContactUs.js';
+import './Footer.scss';
 
 const Footer = () => {
 	return (
 		<div className="footer-container">
 			<Connect />
+			<ContactUs />
 		</div>
 	)
 }
