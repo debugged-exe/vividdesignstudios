7f6840d9691fb08be7fedc9b3762bb068667cc71
harshika6 <harshikasmishra@gmail.com>
2021-04-08 15:00:46 +0530

    categories layout

--- src/Components/Categories/CategoriesLayout/DisplayCategories.js
+++ src/Components/Categories/CategoriesLayout/DisplayCategories.js
@@ -10,9 +10,7 @@ const DisplayCategories=({products})=>  {
 					<div className='flex flex-wrap'>
 					{
             products.map((data)=>(
-
-               <img src={data.img} height='300' width='300'/>
-
+               <img src={data.img} className=' pa3' height='400' width={data.width}/>
             ))
 				  }
 					</div>
@@ -21,3 +19,37 @@ const DisplayCategories=({products})=>  {
 }
 
 export default DisplayCategories;
+	//
+  // <section class="overflow-hidden text-gray-700 body-font">
+  //           <div class="container px-5 py-2 mx-auto lg:pt-24 lg:px-32">
+  //           <div class="flex flex-wrap -m-1 md:-m-2">
+  //               <div class="flex flex-wrap w-1/2">
+  //                   <div class="w-1/2 p-1 md:p-2">
+  //                       <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+  //                           src="https://dummyimage.com/500x300/F3F4F7/8693ac">
+  //                   </div>
+  //                   <div class="w-1/2 p-1 md:p-2">
+  //                       <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+  //                           src="https://dummyimage.com/501x301/F3F4F7/8693ac">
+  //                   </div>
+  //                   <div class="w-full p-1 md:p-2">
+  //                       <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+  //                           src="https://dummyimage.com/600x360/F3F4F7/8693ac">
+  //                   </div>
+  //               </div>
+  //               <div class="flex flex-wrap w-1/2">
+  //                   <div class="w-full p-1 md:p-2">
+  //                       <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+  //                           src="https://dummyimage.com/601x361/F3F4F7/8693ac">
+  //                   </div>
+  //                   <div class="w-1/2 p-1 md:p-2">
+  //                       <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+  //                           src="https://dummyimage.com/502x302/F3F4F7/8693ac">
+  //                   </div>
+  //                   <div class="w-1/2 p-1 md:p-2">
+  //                       <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+  //                           src="https://dummyimage.com/503x303/F3F4F7/8693ac">
+  //                   </div>
+  //               </div>
+  //           </div>
+  //       </section>
--- src/Components/Databases/Hotel.js
+++ src/Components/Databases/Hotel.js
@@ -1,12 +1,23 @@
 const hotel=[
   {
     img:" https://images.unsplash.com/photo-1572803090136-5247de70532f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDl8fGhvdGVsJTIwaW50ZXJpb3J8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+    height:'500px',
+    width:'500px',
   },
   {
     img:"https://images.unsplash.com/photo-1556911220-bff31c812dba?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8a2l0Y2hlbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+    height:'501px',
+    width:'301px',
   },
   {
     img:"https://images.unsplash.com/flagged/photo-1556438758-8d49568ce18e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mjd8fGhvdGVsJTIwaW50ZXJpb3J8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+    height:'600',
+    width:'460',
+  },
+  {
+    img:"https://images.unsplash.com/flagged/photo-1556438758-8d49568ce18e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mjd8fGhvdGVsJTIwaW50ZXJpb3J8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+    height:'300',
+    width:'361',
   }
 ];
 export default hotel;
